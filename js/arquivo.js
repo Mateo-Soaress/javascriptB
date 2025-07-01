948d5b32cb796723944b98270db7cf31baba3de2
Mateo-Soaress <mateosoares.escola@gmail.com>
2025-07-01 12:25:13 -0300

    Aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,26 @@
+var global = "variável global";
+console.log(global);
+
+function exemplo2() {
+    var local = "variável local";
+    console.log(local);
+}
+exemplo2();
+
+let x = 10;
+if (x > 5) {
+
+    var y = 20;
+    console.log("Valor de y: " + y);
+
+}
+
+const _constante = "texto";
+_constante = "outro texto";
+console.log(_constante);
+
+console.log(y)
+
+function saudacao() {
+    alert("Bem-vindo ao nosso site (Dnv....)");
+}
